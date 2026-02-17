bb17fec852317bde98d802de37a8a6df45aa7a81
kurf-ubuntudesktop <daiki.kabuyama.dev@gmail.com>
2026-02-17 20:09:22 +0900

    Add: Add 007-a answer

---
+++ week2/Tue_007_a.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+#define rep(i,n) for(long long i = 0 ; i < (long long) n ; i++)
+#define ll long long
+
+int main(){
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> m(n);
+    vector<vector<ll>> w(n, m);
+    rep(i, n){
+        rep()
+    }
+    ll mn = *min_element(e.begin(), e.end());
+    ll total = 0;
+    rep(i, m){
+        cin >> c[i];
+        total += c[i];
+    }
+    cout << mn * total << '\n';
+    return 0;
+}
